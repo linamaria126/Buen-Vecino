caa3427f6ab9cb0928cfc6c162369be2240d3113
Lina Maria Cardona <linamaria126@gmail.com>
2024-02-09 19:23:53 -0500

    add form

---
+++ src/front/js/pages/registration.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { Context } from "../store/appContext";
+
+export const Registration = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div className="container pt-5">
+      <form>
+        <h2 className="fw-bold">Crear Perfil - Unidad Residencial</h2>
+
+        <i class="fa-solid fa-circle-info d-inline-block fa-2x p-2"></i>
+        <h3 className="fw-bold py-5 d-inline-block"> Información General</h3>
+
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" for="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
